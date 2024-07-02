d6890415cd467ebfca69ac42d6fb38e2f2d0a006
alleappio <allevanny@gmail.com>
2024-07-02 12:38:05 +0200

    edit general params

--- progettoAppio/general_params.py
+++ progettoAppio/general_params.py
@@ -1,6 +1,9 @@
+VERBOSE = False
 TARGET = 'critical_temp'
 FEATURE_CORRELATION_THRESHOLD = 0.5
 FILENAME_SAVE_METRICS = "log/log_metrics"
 STRATEGIES_LIST = ['LinearRegression', 'KNeighborsRegressor', 'DecisionTreeRegressor', 'SVR']
-# FEATURE_SELECTION_METHOD = 'filter'
+#FEATURE_SELECTION_METHOD = 'filter'
 FEATURE_SELECTION_METHOD = 'wrapper'
+FEATURE_SELECTION_CUT_STEP = 5
+FEATURE_SELECTION_CUT_DROP_NUM = 5 

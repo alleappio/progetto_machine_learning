d070d5814533635f6483ad34a112e87f52288067
alleappio <allevanny@gmail.com>
2024-07-04 19:04:09 +0200

    edit prepare: remove validation dataset

--- progettoAppio/prepare.py
+++ progettoAppio/prepare.py
@@ -8,11 +8,9 @@ class PrepareData:
     def __init__(self, data, target = 'critical_temp', normalization=True):
         self.data = data
         self.target = target
-        self.train_data, temp_data = train_test_split(self.data, test_size=0.4)
-        self.val_data, self.test_data = train_test_split(temp_data, test_size=0.5)
+        self.train_data, self.test_data = train_test_split(self.data, test_size=0.4)
         
         self.train_data = self.train_data.reset_index(drop=True)
-        self.val_data = self.val_data.reset_index(drop=True)
         self.test_data=self.test_data.reset_index(drop=True)
         
         self.split_X_y()
@@ -22,8 +20,6 @@ class PrepareData:
     def split_X_y(self):
         self.y_train = self.train_data[self.target]
         self.X_train = self.train_data.drop(self.target, axis=1)
-        self.y_val = self.val_data[self.target]
-        self.X_val = self.val_data.drop(self.target, axis=1)
         self.y_test = self.test_data[self.target]
         self.X_test = self.test_data.drop(self.target, axis=1)
 
@@ -33,17 +29,11 @@ class PrepareData:
         mms.fit(self.X_train)
         self.X_train[self.X_train.columns] = mms.transform(self.X_train[self.X_train.columns])
         
-        mms.fit(self.X_val)
-        self.X_val[self.X_val.columns] = mms.transform(self.X_val[self.X_val.columns])
-        
         mms.fit(self.X_test)
         self.X_test[self.X_test.columns] = mms.transform(self.X_test[self.X_test.columns])
     
     def get_train_data(self):
         return self.X_train, self.y_train
-
-    def get_val_data(self):
-        return self.X_val, self.y_val
     
     def get_test_data(self):
         return self.X_test, self.y_test
